21fac3ae01f02c6bdac0b459d96f59bc9f6fb6f1
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-01-09 01:48:27 +0300

    Intro to Javascript

---
+++ Modern Javascript Series/scripts/myscripts.js
@@ -0,0 +1,6 @@
+document.write("This page is Copyright " + new Date().
+getFullYear());
+
+let firstName; //Variable for first name
+firstName=prompt('Please tell me yoyr first name')
+document.write('Welcome to my first website,'+first)
